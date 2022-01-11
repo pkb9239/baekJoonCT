743f826c609a82a0a21a61288abd48668945d89f
Kyungbin <pkb8839@naver.com>
2022-01-11 13:43:20 +0900

    [백준[ 10869번 수정
    Math.floor 메소드 추가

--- b10869/app.js
+++ b10869/app.js
@@ -12,6 +12,6 @@ function solution(A, B) {
     console.log(A + B);
     console.log(A - B);
     console.log(A * B);
-    console.log(A / B);
+    console.log(Math.floor(A / B));
     console.log(A % B);
 }
