7bca0d14dd87dd9a9bc51fbcd6b2fff942dd8298
Kyungbin <pkb8839@naver.com>
2022-03-17 23:58:42 +0900

    Revert "[백준 코딩테스트]"
    This reverts commit d5bd902ad80d1011a62c5e0b8eca1c2727d02dea.

--- b10818/app1.js
+++ b10818/app1.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
+const input1 = input[0];
 const testArray = input[1].split(" ").map((item) => +item);
 
-solution(testArray);
+solution(input1, testArray);
 
-function solution(testarray) {
-    const min = testarray.sort((a, b) => a - b)[0];
-    const max = testarray.sort((a, b) => b - a [0]);
-    console.log(`${min} ${max}`);
+function solution(N, testarray) {
+    const answer = [];
+   const a = Math.max([...testarray]);
+   const b = Math.min([...testarray]);
+   answer.push(b[0]);
+   answer.push(a[0]);
+   answer.join(' ');
+   console.log(answer);
 }
