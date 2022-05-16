9f60400a4e31c56e2f8ddc26f1148f2124842de4
Kyungbin <pkb8839@naver.com>
2022-05-17 02:40:17 +0900

    백준문제아님 코드내용추가/수정 2. 유효한 팰린드롬 3. 숫자만추출

--- section3/2.js
+++ section3/2.js
@@ -1,6 +1,7 @@
 function solution(s) {
     let answer = 'YES';
     s = s.toLowerCase().replace(/[^a-z]/g, '');/// 소문자a-z까지가 아닌것들을 찾아 빈문자열로 바꿈 
+    console.log(s);
     if (s.split('').reverse().join('') !== s) return "NO";
     return answer;
 }
--- section3/3.js
+++ section3/3.js
@@ -1,14 +1,23 @@
 function solution(s) {
-    let answer='';
-   // 대문자아스키코드는 65~90  소문자 97~122
-    for (let x of s) {
-        let num = x.charCodeAt();
-        if (65<= num <= 90 && 97 <= num <= 122) {
-            answer = x.replace('num', '');
-        }  
-    }
-    return answer;
+    let answer="";
+    for (let x of s) {   //isNaN 숫자가 아니면 
+        if (!isNaN(x)) answer+=x;
+    } 
+    
+    return parseInt(answer);
 }
 
 const str = 'g0en2T0s8eSoft';
 console.log(solution(str));
+
+// function solution(s) {
+//     let answer=0;
+//     for (let x of s) {  
+//         if (!isNaN(x)) answer=answer*10+ Number(x);
+//     } 
+    
+//     return answer;
+// }
+
+// const str = 'g0en2T0s8eSoft';
+// console.log(solution(str));
