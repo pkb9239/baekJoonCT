5d59b30fd3906d7792f3e0d8242b24f39f4d97f6
Kyungbin <pkb8839@naver.com>
2022-06-20 16:50:01 +0900

    [코딩테스트]
    백준문제아님
    정렬과 그리디, 결정알고리즘(이분검색)
    6. 장난꾸러기 현수

--- section7/6.js
+++ section7/6.js
@@ -1,26 +1,11 @@
 function solution(item) {
-    let 현수;
-    let 짝꿍;
-    let b = 0;
-
-    for (let i = 0; i < item.length; i++) {
-        if (item[i] > b) {
-            b = item[i];
-        } else {
-            현수 = i;
-            break;
-        }
-    }
-    for (let i = 현수; i < item.length; i++) {
-        if (item[i] > b) {
-            b = item[i];
-        } else {
-            짝꿍 = i + 현수 - 1
-        }
-    }
-
-  return (`${현수} ${짝꿍}`);
+   let answer = [];
+   let copy = [...item];
+   const sort = copy.sort((a, b) => a - b);
+   for (let i = 0; i < item.length; i++) {
+    if (item[i] !== sort[i]) answer.push(i+1);
+   }
+  return answer;
 }
-
-const item = [120, 125, 152, 130, 135, 143, 127, 160];
+const item = [120, 130, 150, 150, 130, 150];
 console.log(solution(item));
