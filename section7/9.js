f60341e7e027aa8077551ea43432655f47627ece
Kyungbin <pkb8839@naver.com>
2022-06-21 23:20:07 +0900

    [코딩테스트]
    백준문제아님
    정렬과 그리디, 결정알고리즘(이분검색)
    9. 결혼식

--- section7/9.js
+++ section7/9.js
@@ -1,11 +1,22 @@
 function solution(arr) {
-    let answer= 0;
-    const sort = arr.sort((a, b) => {
-        return a[0] - b[0];
+    let answer = Number.MIN_SAFE_INTEGER;
+    let cnt = 0;
+    let t_line = [];
+    for (let x of arr) {
+        t_line.push([x[0], 's']);
+        t_line.push([x[1], 'e']);
+    }
+    t_line.sort((a, b) => {
+        if (a[0] === b[0]) return a[1].charCodeAt() - b[1].charCodeAt();
+        else return a[0] - b[0];
     });
-    for (let i = 0; i < sort.length; i++) {
-        for (let j = 1 + i; j < sort.length -1 - i; j++) {
-            if ( sort[j][0] < sort[i][1] && sort[i][1]< sort[j][1]) answer++;
+    for (let i = 0; i < t_line.length; i++) {
+        if (t_line[i][1] === 's') {
+            cnt++;
+            if (answer < cnt) answer = cnt;
+        } else if (t_line[i][1] === 'e') {
+            cnt--;
+            if (answer < cnt) answer = cnt;
         }
     }
     return answer;
@@ -15,7 +26,7 @@ const arr = [
     [14, 18],
     [12, 15],
     [15, 20],
-    [20, 30],
+    [15, 30],
     [5, 14]
 ];
 
