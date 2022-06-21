5d6d62f9ed60cb24beee2d021685089e27af07e4
Kyungbin <pkb8839@naver.com>
2022-06-22 02:13:59 +0900

    [코딩테스트]
    백준문제아님
    정렬과 그리디, 결정알고리즘(이분검색)
    10. 이분탐색 (이분탐색으로풀었음)

--- section7/10.js
+++ section7/10.js
@@ -11,14 +11,21 @@
 //     return answer;
 // }
 
+// 이분 탐색으로 풀기
 function solution(item, m) {
     let answer;
-    const newItem = [...item];
-    newItem.push(m);
-    newItem.sort((a, b) => a - b);
-    const set = [...new Set(newItem)];
-    console.log(set);
-    answer = set.indexOf(m) + 1;
+    item.sort((a, b) => a - b);
+    let rt = item.length - 1;
+    let lt = 0;
+    while (lt <= rt) {
+        let mid = parseInt((lt + rt) / 2);
+        if (item[mid] === m) {
+            answer= mid + 1;
+            break;
+        } else if (item[mid] > m) rt = mid - 1
+        else lt = mid + 1;
+    }
+
     return answer;
 }
 
