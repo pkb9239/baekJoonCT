f86d78e11ee84ed57256c8b2a56700ea107b76eb
Kyungbin <pkb8839@naver.com>
2022-06-26 15:21:34 +0900

    [코딩테스트]
    백준문제아님
    재귀함수와 완전탐색(DFS:깊이우선탐색)
    5. 합이 같은 부분집합

--- section8/5.js
+++ section8/5.js
@@ -1,14 +1,21 @@
-function solution(arr) {
+//  합이 같은 부분집합(DFS: 아마존 인터뷰)
+
+function solution(arr) {  
     let answer = 'NO';
+    let flag = 0;
     const total = arr.reduce((pre, cur) => pre + cur, 0);
     let n = arr.length;
+
     function DFS(L, sum) {
-        if (L===n) {
-            if ((total - sum) === sum) answer="YES";
+        if (L === n) {
+            if ((total - sum) === sum) { 
+                answer = 'YES';
+                flag = 1;
+            }
         }
         else {
-            DFS(L+1, sum+arr[L]);
-            DFS(L+1, sum);
+            DFS(L + 1, sum + arr[L]);
+            DFS(L + 1, sum);
         }
     }
     DFS(0, 0);
