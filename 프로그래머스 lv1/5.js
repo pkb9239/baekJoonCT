747d8b1666c8bc2894daa94708ca9d277641067c
Kyungbin <pkb8839@naver.com>
2022-06-26 18:44:50 +0900

    lv1. k번째 수

---
+++ 프로그래머스 lv1/5.js
@@ -0,0 +1,12 @@
+function solution(array, commands) {
+    let answer = [];
+    for (let i = 0; i < commands.length; i++) {
+        let list = array.slice(commands[i][0]-1, commands[i][1]).sort((a, b) => a - b);
+        answer.push(list[commands[i][2] - 1]);
+    }
+    
+    return answer;
+}
+const array = [1, 5, 2, 6, 3, 7, 4];
+const commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]];
+console.log(solution(array, commands));
