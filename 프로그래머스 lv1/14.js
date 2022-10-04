7ee4f241df3b773d13e68945854ba252e386ea0c
Kyungbin <pkb8839@naver.com>
2022-10-04 21:13:36 +0900

    lv1. 약수의 합

---
+++ 프로그래머스 lv1/14.js
@@ -0,0 +1,11 @@
+// 약수의 합 
+function solution(n) {
+    let answer = 0;
+    for(let i = 1; i <= n; i++){
+        if(n % i === 0){
+            answer += i 
+        }
+    }
+    return answer;
+}
+// const n = 5;
