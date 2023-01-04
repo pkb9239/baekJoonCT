af4fd43f8056a96977680d130451d91aff79556c
kyungbin <pkb8839@naver.com>
2023-01-04 13:47:53 +0900

    lv1. 소수 찾기
    math.sqrt 사용해서 소수판별해서 품

---
+++ 프로그래머스 lv1/52.js
@@ -0,0 +1,18 @@
+// 소수 찾기
+
+function isPrime(x) {
+    for (let i = 2; i <= Math.sqrt(x); i++) {
+        if (x % i === 0) return false;
+    }
+    return true;
+}
+
+function solution(n) {
+    let answer = 0;
+    for (let i = 2; i <= n; i++) {
+        if (isPrime(i)) answer++;
+    }
+    return answer;
+}
+const n = 10;
+console.log(solution(n));
